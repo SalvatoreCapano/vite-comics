e7a4e62954b7f46a44f5116dd6fdf8aa5886f305
Salvatore Capano <capano_salvatore@libero.it>
2023-02-02 18:13:14 +0100

    Main Completato

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,11 +10,142 @@ export default {
 
 
 <template>
+
+    <main>
+        
+        <!-- Main Content -->
+        <section class="mainContentSection">
+            <div class="container">
+
+                <span>-- Content goes here --</span>
+
+            </div> <!-- /container-->
+        </section> <!-- /mainContentSection-->
+
+
+        <section class="actionsSection">
+            <div class="container">
+                
+                <ul>
+
+                    <li>
+                        <a href="#">
+                            <div class="imgContainer">
+                                <img src="../assets/buy-comics-digital-comics.png" alt="Digital Comics">
+                            </div> <!-- /imgContainer-->
+                            <span class="textContainer">
+                                digital comics
+                            </span> <!-- /textContainer-->
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <div class="imgContainer">
+                                <img src="../assets/buy-comics-merchandise.png" alt="DC Merchandise">
+                            </div> <!-- /imgContainer-->
+                            <span class="textContainer">
+                                dc merchandise
+                            </span> <!-- /textContainer-->
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <div class="imgContainer">
+                                <img src="../assets/buy-comics-subscriptions.png" alt="Subscription">
+                            </div> <!-- /imgContainer-->
+                            <span class="textContainer">
+                                subscription
+                            </span> <!-- /textContainer-->
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <div class="imgContainer">
+                                <img src="../assets/buy-comics-shop-locator.png" alt="Comic Shop Locator">
+                            </div> <!-- /imgContainer-->
+                            <span class="textContainer">
+                                comic shop locator
+                            </span> <!-- /textContainer-->
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <div class="imgContainer">
+                                <img src="../assets/buy-dc-power-visa.svg" alt="DC Power Visa">
+                            </div> <!-- /imgContainer-->
+                            <span class="textContainer">
+                                dc power visa
+                            </span> <!-- /textContainer-->
+                        </a>
+                    </li>
+
+                </ul>
+
+            </div> <!-- /container-->
+        </section> <!-- /actionsSection-->
+
+    </main>
     
 </template>
 
 
 <style lang="scss" scoped>
+    @import "../styles/main.scss";
     @import "../styles/partials/mixin.scss";
+
+    .mainContentSection {
+        background-color: $dark-color;
+        padding: $spacing-large;
+
+        span {
+            font-size: 2rem;
+            font-weight: bold;
+            color: white;
+        }
+    }
+
+    .actionsSection {
+        background-color: $brand-color;
+        padding: $spacing-large;
+        
+        .container {
+            padding: 0 $spacing-large;
+        }
+
+        ul {
+            @include flexRowSpaced;
+            gap: 3rem;
+            list-style: none;
+
+            li a {
+                @include flexRowSpaced;
+                gap: 0.4rem;
+                text-decoration: none;
+                white-space: nowrap;
+
+                .imgContainer {
+                    height: 60px;
+                    width: 60px;
+
+                    img {
+                        max-width: 100%;
+                        max-height: 100%;
+                    }
+                }
+
+                .textContainer {
+                    color: #ffffff;
+                    text-transform: uppercase;
+                    font-size: 0.8rem;
+                    font-weight: 600;
+                }
+            }
+        }
+
+    }
 
 </style>
